8d9feb027c6211e14f63c2851c4687ee089f38ec
IVAN <ivanbatur5g@gmail.com>
2020-10-06 19:06:37 +0200

    Hooks finished

--- cv-app/src/App.js
+++ cv-app/src/App.js
@@ -1,14 +1,16 @@
 import React from "react";
 import PersonalInfo from "./components/PersonalInfo";
 import Education from "./components/Education";
-/* import Experience from "./components/Experience"; */
+
 import "./App.css";
+import Experience from "./components/Experience";
 function App() {
   return (
     <div className="app">
       <h2>Curriculum Vitae</h2>
       <PersonalInfo />
       <Education />
+      <Experience />
     </div>
   );
 }
--- cv-app/src/components/Experience.js
+++ cv-app/src/components/Experience.js
@@ -5,5 +5,62 @@ function Experience(props) {
   const [responsibilities, setResponsibilities] = useState("");
   const [startDate, setStartDate] = useState("");
   const [endDate, setEndDate] = useState("");
+  const [isEditing, setIsEditing] = useState(true);
+
+  if (isEditing) {
+    return (
+      <div>
+        <h2>Experience</h2>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            setIsEditing(false);
+          }}
+        >
+          <input
+            placeholder="Company"
+            value={company}
+            onChange={(e) => setCompany(e.target.value)}
+            type="text"
+          />
+          <input
+            placeholder="Role"
+            value={role}
+            onChange={(e) => setRole(e.target.value)}
+            type="text"
+          />
+          <input
+            placeholder="Responsibilities"
+            value={responsibilities}
+            onChange={(e) => setResponsibilities(e.target.value)}
+            type="text"
+          />
+          <input
+            value={startDate}
+            onChange={(e) => setStartDate(e.target.value)}
+            type="date"
+          />
+          <input
+            value={endDate}
+            onChange={(e) => setEndDate(e.target.value)}
+            type="date"
+          />
+          <button>Submit</button>
+        </form>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <h2>Experience</h2>
+        <p>Company: {company}</p>
+        <p>Role: {role}</p>
+        <p>Responsibilities: {responsibilities}</p>
+        <p>Start date: {startDate}</p>
+        <p>End date: {endDate}</p>
+        <button onClick={(e) => setIsEditing(true)}>Edit</button>
+      </div>
+    );
+  }
 }
 export default Experience;
